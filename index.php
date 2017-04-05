409335329223371c184b2c0006d975a722f6960e
Alver Grisales <viga.23@hotmail.com>
2017-04-04 23:20:52 -0500

    Add class for the form with bootstrap

--- index.php
+++ index.php
@@ -8,14 +8,23 @@
 	<title>Login</title>
 </head>
 <body>
-<div>
-	<form action="login.php" method="post">
-		<label>Correo Electronico:</label>
-		<input type="email" name="email" required="">
-		<label>Contraseña:</label>
-		<input type="password" name="password" required="">
-		<button type="submit">Iniciar Session</button>
-	</form>
+<div class="container">
+	<div class="row">
+		<div class="col-md-12">
+			<form action="login.php" method="post">
+				<div class="form-group">
+					<label for="correo_electronico">Correo Electronico:</label>
+					<input type="email" name="email" id="correo_electronico" required="" class="form-control">
+				</div>
+				<div class="form-group">
+					<label for="contrasena">Contraseña:</label>
+					<input type="password" name="password" id="contrasena" required="" class="form-control">
+				</div>
+		
+				<button type="submit" class="btn btn-primary">Iniciar Session</button>
+			</form>
+		</div>
+	</div>
 </div>
 <script type="text/javascript" src="public/plugins/jquery-3.2.0.min.js"></script>
 <script type="text/javascript" src="public/plugins/bootstrap-3.3.7-dist/js/bootstrap.js"></script>
