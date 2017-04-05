eac1366f4bf5c5945c9fa65c1d79802eb899ec6d
Alver Grisales <viga.23@hotmail.com>
2017-04-04 22:45:07 -0500

    Add index form for login with bootstrap

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <!DOCTYPE html>
-<html>
+<html lang="es">
 <head>
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="public/plugins/bootstrap-3.3.7-dist/css/bootstrap.css">
 	<title>Login</title>
 </head>
 <body>
@@ -13,5 +17,7 @@
 		<button type="submit">Iniciar Session</button>
 	</form>
 </div>
+<script type="text/javascript" src="public/plugins/jquery-3.2.0.min.js"></script>
+<script type="text/javascript" src="public/plugins/bootstrap-3.3.7-dist/js/bootstrap.js"></script>
 </body>
 </html>
