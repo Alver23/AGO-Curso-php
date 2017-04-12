64ba7930a50226eaeddea6dcc07f98157ff50ccf
Alver Grisales <viga.23@hotmail.com>
2017-04-11 23:40:18 -0500

    Create login HTML with CSS

--- index.php
+++ index.php
@@ -1,32 +1,22 @@
 <!DOCTYPE html>
-<html lang="es">
+<html>
 <head>
-	<meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" href="public/plugins/bootstrap-3.3.7-dist/css/bootstrap.css">
 	<title>Login</title>
+	<link rel="stylesheet" type="text/css" href="public/css/style.css?v=0.1.0">
 </head>
 <body>
 <div class="container">
-	<div class="row">
-		<div class="col-md-12">
-			<form action="login.php" method="post">
-				<div class="form-group">
-					<label for="correo_electronico">Correo Electronico:</label>
-					<input type="email" name="email" id="correo_electronico" required="" class="form-control">
-				</div>
-				<div class="form-group">
-					<label for="contrasena">Contraseña:</label>
-					<input type="password" name="password" id="contrasena" required="" class="form-control">
-				</div>
-		
-				<button type="submit" class="btn btn-primary">Iniciar Session</button>
-			</form>
+	<form method="post" action="">
+		<div class="form-control">
+			<label>Email:</label>
+			<input type="email" name="email">
 		</div>
-	</div>
+		<div class="form-control">
+			<label>Password</label>
+			<input type="password" name="password">
+		</div>
+		<button type="submit" class="btn btn-blue">Login</button>
+	</form>
 </div>
-<script type="text/javascript" src="public/plugins/jquery-3.2.0.min.js"></script>
-<script type="text/javascript" src="public/plugins/bootstrap-3.3.7-dist/js/bootstrap.js"></script>
 </body>
 </html>
--- login.php
+++ login.php
@@ -1,15 +1,11 @@
 <?php
 session_start();
-require 'models/User.php';
-if ($_POST) {
-	$email = !empty($_POST['email']) ? $_POST['email'] : null;
-	$password = !empty($_POST['password']) ? $_POST['password']: null;
-	if (empty($email) || empty($password)) {
-		header('location: index.php');
-	}
-	$userData = login($email, $password);
-	if (count($userData) > 0) {
-		$_SESSION['user_data'] = $userData;
-		header('location: home.php');
+if (!empty($_POST)) {
+	$email = $_POST["correo_electronico"];
+	$password = $_POST["Contrasena"];
+	if ($email === "alver.grisales@hotmail.com" && $password === "123456") {
+		header("location: home.php");
+	} else {
+		header("location: index.php");
 	}
 }
